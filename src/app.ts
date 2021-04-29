ecd0c123a73b34c704071da53b340201232f6757
Manuel Avelar <me@mavelar.com>
2021-04-29 08:58:01 -0600

    feat: Code Coverage

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+
+import logger from 'koa-logger';
+import json from 'koa-logger';
+import bodyParser from 'koa-bodyparser';
+
+const app = new Koa();
+const router = new Router();
+
+const port : number = 3002;
+
+interface HelloRequest {
+    name: string;
+}
+
+// hello world
+router.post('/', async (ctx, next) => {
+    const data = <HelloRequest>ctx.request.body;
+    ctx.body = { name: data.name};
+    await next();
+})
+
+// Middleware
+app.use(json());
+app.use(logger());
+app.use(bodyParser());
+
+// Routes
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(port, () => {
+    console.log(`Koa Started`);
+    console.log(`Listening port ${port}`);
+})
