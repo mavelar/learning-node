c0c09f0cf80556117accf6a8ec920e819c180f80
Manuel Avelar <me@mavelar.com>
2021-05-10 11:56:30 -0600

    test fixed

--- jest.config.ts
+++ jest.config.ts
@@ -9,5 +9,13 @@ export default {
   testEnvironment: "node",
   coveragePathIgnorePatterns: [
     "/node_modules/"
-  ]
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
 };
